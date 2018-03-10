686701751bba8b7c3b869bd70311c4b7a5c19c02
Vernon Mullen <vernonmu@gmail.com>
2018-03-10 00:22:40 -0600

    readded background image. added angular files. updated sass files. updated gulp routes. created public dir.

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('/public/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('/public/css'));
 });
 
 gulp.task('sass:watch', function(){
-  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('/public/sass/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass', 'sass:watch']);
---
+++ public/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('app', ['ui.router'])
+// .config(function($stateProvider, $urlRouterProvider){
+//   $stateProvider
+//   .state(
+//     'intro', {
+//       url: '/',
+//       templateUrl: './comp/home.html'
+//     }
+//   )
+//   .state(
+//     'service', {
+//       url: '/service',
+//       templateUrl: './comp/service.html'
+//     }
+//   )
+//   .state(
+//     'contact', {
+//       url: '/contact',
+//       templateUrl: './comp/contact.html'
+//     }
+//   )
+//   $urlRouterProvider.otherwise('/')
+// })

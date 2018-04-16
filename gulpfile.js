4d064976510996f252cb042b8d7f85aed7116549
Vernon Mullen <vernonmu@gmail.com>
2018-04-16 12:52:05 -0500

    fixed gulp targets.

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,15 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('/public/sass/**/*.scss')
+  console.log('starting gulp sass');
+  return gulp.src('./public/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('/public/css'));
+    .pipe(gulp.dest('./public/css'));
+    console.log('done gulping sass');
 });
 
 gulp.task('sass:watch', function(){
-  gulp.watch('/public/sass/*.scss', ['sass']);
+  gulp.watch('./public/sass/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass', 'sass:watch']);

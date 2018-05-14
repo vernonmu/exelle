7428b6645e1dff96d0b004e3819dff50dd62e6c7
Vernon Mullen <vernonmu@gmail.com>
2018-05-14 14:00:08 -0500

    added booking modal form and success message.

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -1,3 +1,22 @@
-app.controller('mainCtrl', function($scope){
+app.controller('mainCtrl', function($scope, $timeout){
   console.log('test control');
+
+  $scope.createBooking = function(booking) {
+    console.log('create booking fn has : ', booking);
+    $scope.showBooking = false;
+    $scope.showSuccessMessage = true;
+    $scope.booking = {};
+    $scope.bookingForm.$setPristine();
+
+    $timeout(function(){
+      if ($scope.showSuccessMessage == true) {
+        $scope.showSuccessMessage = false;
+        console.log('we are done counting now!');
+      }
+      else {
+        console.log('we have already closed the box, man.');
+      }
+    }, 10000)
+
+  }
 })
